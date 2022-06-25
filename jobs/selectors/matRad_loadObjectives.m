110e56e97a733b26324d76856214031ae98f8c12
Camilo Sevilla <acsevillam@gmail.com>
2022-06-25 21:40:45 +0200

    New implementation of Cheap-Minimax gradient calculation

--- jobs/selectors/matRad_loadObjectives.m
+++ jobs/selectors/matRad_loadObjectives.m
@@ -353,7 +353,7 @@ switch radiationMode
                         
                         % Heart
                         cst{4,5}.Priority = 2; % overlap priority for optimization - a lower number corresponds to a higher priority
-                        cst{4,6}{1} = struct(DoseObjectives.matRad_MeanDose(10,0));
+                        cst{4,6}{1} = struct(DoseObjectives.matRad_MeanDose(2.5,0));
                         cst{4,6}{1}.robustness  = 'none';
                         
                     case '5'
@@ -366,7 +366,7 @@ switch radiationMode
                         
                         % Heart
                         cst{4,5}.Priority = 2; % overlap priority for optimization - a lower number corresponds to a higher priority
-                        cst{4,6}{1} = struct(DoseObjectives.matRad_MeanDose(5,0));
+                        cst{4,6}{1} = struct(DoseObjectives.matRad_MeanDose(1,0));
                         cst{4,6}{1}.robustness  = 'none';
                         
                     case '6'
@@ -379,7 +379,7 @@ switch radiationMode
                         
                         % Heart
                         cst{4,5}.Priority = 2; % overlap priority for optimization - a lower number corresponds to a higher priority
-                        cst{4,6}{1} = struct(DoseObjectives.matRad_MeanDose(2.5,0));
+                        cst{4,6}{1} = struct(DoseObjectives.matRad_MeanDose(0.5,0));
                         cst{4,6}{1}.robustness  = 'none';
                         
                 end
