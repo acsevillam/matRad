be2c9e025af9713e6ab539612fbb51337bab5c31
Camilo Sevilla <acsevillam@gmail.com>
2022-09-22 11:21:42 -0500

    Adding new optimization model INTERVAL3

--- jobs/matRad_photonRobust.m
+++ jobs/matRad_photonRobust.m
@@ -506,8 +506,10 @@ end
 % the clinical objectives and constraints underlying the radiation
 % treatment. Once the optimization has finished, trigger once the GUI to
 % visualize the optimized dose cubes.
+arallel.internal.ui.MatlabProfileManager.removeProfile('profile1');
+parallel.internal.ui.MatlabProfileManager.removeProfile('profile1_Import');
 profile_master = parallel.importProfile('profile1.mlsettings');
-p=parpool(profile_master);
+p=parpool(profile_master,32);
 
 profile on;
 now3 = tic();
