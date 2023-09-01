f8423829503f7e83e31ed05f97717efe77c2eaec
acsevillam <34175617+acsevillam@users.noreply.github.com>
2023-08-31 21:32:28 -0500

    Adding dose pulling for INTERVAL2/3 model

--- analysis/DeltaMap_analysis1_article.m
+++ analysis/DeltaMap_analysis1_article.m
@@ -7,36 +7,36 @@ matRad_rc
 param.logLevel=1;
 %defaultRootPath = matRad_cfg.matRadRoot;
 defaultRootPath = '\\compute-0-0\workspace';
-%defaultRootPath = 'C:\Users\acsevillam\workspace';
 job_folder='job1';
 radiationMode='photons';
 description='prostate';
 caseID='3790'; % 3482 3648 3782 3790 3840
-robustness_approach = 'nominal';
-robustness='none'; % none COWC COWC2 c-COWC c-COWC2 INTERVAL2 INTERVAL3
-plan_target='PTV'; % CTV PTV
+robustness_approach = 'robust';
+robustness='INTERVAL3'; % none COWC COWC2 c-COWC c-COWC2 INTERVAL2 INTERVAL3
+plan_target='CTV'; % CTV PTV
 plan_beams='9F';
 plan_objectives='4';
 shiftSD='5x10x5';
-scen_mode='nomScen'; % nomScen impScen5 impScen_permuted_truncated5 impScen7 impScen_permuted_truncated7
+scen_mode='impScen5'; % nomScen impScen5 impScen_permuted_truncated5 impScen7 impScen_permuted_truncated7
 wcFactor=1.0;
 beta1=1/13;
-p2=12;
+p2=1;
 beta2=p2/13;
 theta1=1.0;
-theta2=0.1;
-
-output_folder = ['output' filesep radiationMode filesep description filesep caseID filesep robustness ...
-    filesep plan_target filesep plan_beams filesep plan_objectives filesep shiftSD filesep scen_mode ];
+theta2=0.0;
 
 %output_folder = ['output' filesep radiationMode filesep description filesep caseID filesep robustness ...
-%    filesep plan_target filesep plan_beams filesep plan_objectives filesep scen_mode filesep num2str(wcFactor) filesep num2str(beta1) '_to_' num2str(beta2) ];
+%    filesep plan_target filesep plan_beams filesep plan_objectives filesep shiftSD filesep scen_mode ];
 
 %output_folder = ['output' filesep radiationMode filesep description filesep caseID filesep robustness ...
 %    filesep plan_target filesep plan_beams filesep plan_objectives filesep shiftSD filesep scen_mode filesep num2str(wcFactor) filesep num2str(beta1) '_to_' num2str(beta2) ];
 
+output_folder = ['output' filesep radiationMode filesep description filesep caseID filesep robustness ...
+    filesep plan_target filesep plan_beams filesep plan_objectives filesep shiftSD filesep scen_mode filesep num2str(wcFactor) filesep num2str(theta1) filesep num2str(theta2) ];
+
+
 %foldername = [defaultRootPath filesep '../../JOBS/cminimax2/1/job4' filesep output_folder];
-foldername = [defaultRootPath filesep 'JOBS\apolo\cminimax2\2023-06-11\1' filesep job_folder filesep output_folder];
+foldername = [defaultRootPath filesep 'JOBS\apolo\interval3\apolo\2023-08-30\1' filesep job_folder filesep output_folder];
 listing = dir(foldername);
 filename1=[foldername filesep listing(end).name filesep 'results.mat'];
 filename2=[foldername filesep listing(end).name filesep 'plan.mat'];
@@ -151,13 +151,15 @@ for slice = 23:2:35
     set(robustnessFig1,'PaperPositionMode','auto');
     set(robustnessFig1.Children(4),'XLim',[76.5000 99.5000]);
     set(robustnessFig1.Children(4),'YLim',[70.5000 95.5000]);
-    set(robustnessFig1.Children(4).Title,'String','PTV');
+    set(robustnessFig1.Children(4).Title,'String',strjoin(['interval3','theta1 =',string(theta1),', theta2 =',string(theta2)],' ')); % Nomimal PTV (Margin based) minimax strjoin(['cminimax',string(p2),'of 13'],' ') strjoin(['interval3','theta1:',string(theta1),' and theta2:',string(theta2)],' ')
     set(robustnessFig1.Children(6),'XLim',[76.5000 99.5000]);
     set(robustnessFig1.Children(6),'YLim',[70.5000 95.5000]);
-    set(robustnessFig1.Children(6).Title,'String','PTV');
+    set(robustnessFig1.Children(6).Title,'String',strjoin(['interval3','theta1 =',string(theta1),', theta2 =',string(theta2)],' '));
     %set(robustnessFig1,'PaperSize',[5.8 4.0]);
-    %print(robustnessFig1,[foldername filesep listing(end).name filesep 'dvh_trustband_' robustness_approach '_' num2str(p2) '_13'],'-dpdf','-r0','-fillpage');
-    print(robustnessFig1,[foldername filesep listing(end).name filesep 'robustness_analysis1_' robustness_approach '_' plan_target '_' num2str(slice)],'-dpdf','-r0','-fillpage');
+    %print(robustnessFig1,[foldername filesep listing(end).name filesep 'robust_conformity2_' robustness_approach '_' plan_target '_' num2str(slice)],'-dpdf','-r0','-fillpage');
+    %print(robustnessFig1,[foldername filesep listing(end).name filesep 'robust_conformity2_' robustness_approach '_' plan_target  '_' num2str(p2) '_13' '_' num2str(slice)],'-dpdf','-r0','-fillpage');
+    print(robustnessFig1,[foldername filesep listing(end).name filesep 'robust_conformity2_' robustness_approach '_' plan_target  '_' num2str(theta1) '_' num2str(theta2) '_' num2str(slice)],'-dpdf','-r0','-fillpage');
+
 end
 close all;
 diary off
