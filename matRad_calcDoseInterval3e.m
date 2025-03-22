3f8295c08e76cd49036bf57c174641a55499a1e8
Camilo Sevilla <acsevillam@Camilos-MacBook-Air.local>
2025-03-22 11:26:17 -0500

    Adding new optimization model INTERVAL3

--- matRad_calcDoseInterval3e.m
+++ matRad_calcDoseInterval3e.m
@@ -97,7 +97,7 @@ parfor it=1:numel(targetSubIx)
     
     Ix=targetSubIx(it);
     dij_tmp=cell2mat(cellfun(@(data) data(Ix,:),dij_list,'UniformOutput',false));
-    dij_tmp_weighted = dij_tmp .* scenProb'; % vector-matrix element-wise
+    dij_tmp_weighted = dij_tmp .* scenProb; % vector-matrix element-wise
 
     % Voxel index
     dij_interval_target(it).Ix=Ix;
